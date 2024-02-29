9adb7a8df97017abcfc41c32010b6616a80aa2a7
FabLrc <fabien.laurence33@gmail.com>
2024-02-29 14:29:15 +0100

    Modification du proxy pour prod

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/auth/**",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: `${process.env.PUBLIC_URL}:5000`,
     })
   );
 };
